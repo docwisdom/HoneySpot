ed84204741fabc98be79d1f7f6f73df15bd37e44
docwisdom <docwisdom@MBP-2.local>
2013-08-29 22:31:54 -0700

    Beautify map.js

--- www/js/map.js
+++ www/js/map.js
@@ -1,114 +1,128 @@
 $(document).ready(function (event) {
     max_height();
-    navigator.geolocation.getCurrentPosition(onSuccess, onError, {'enableHighAccuracy' : true, 'timeout' : 20000});
+    navigator.geolocation.getCurrentPosition(onSuccess, onError, {
+        'enableHighAccuracy': true,
+        'timeout': 20000
+    });
     $('#mileSelect input:radio').change(function () {
         max_height();
-        navigator.geolocation.getCurrentPosition(onSuccess, onError, {'enableHighAccuracy' : true, 'timeout' : 20000});
+        navigator.geolocation.getCurrentPosition(onSuccess, onError, {
+            'enableHighAccuracy': true,
+            'timeout': 20000
+        });
     });
 });
 
 //set max height of map block
+
 function max_height() {
     var header = $.mobile.activePage.find("div[data-role='header']:visible");
     var footer = $.mobile.activePage.find("div[data-role='footer']:visible");
     var content = $.mobile.activePage.find("div[data-role='content']:visible:visible");
     var viewport_height = $(window).height();
-    
+
     var content_height = viewport_height - header.outerHeight() - footer.outerHeight();
-    if((content.outerHeight() - header.outerHeight() - footer.outerHeight()) <= viewport_height) {
+    if ((content.outerHeight() - header.outerHeight() - footer.outerHeight()) <= viewport_height) {
         content_height -= (content.outerHeight() - content.height());
-    } 
+    }
     $.mobile.activePage.find('[data-role="content"]').height(content_height);
 }
 
 //set current location
-function onSuccess(position) {       
+
+function onSuccess(position) {
     var minZoomLevel = 15;
     var myLatlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
     var bounds = new google.maps.LatLngBounds();
-    
+
     var map = new google.maps.Map(document.getElementById('map_canvas'), {
         zoom: minZoomLevel,
         center: myLatlng,
         mapTypeControl: false,
         mapTypeId: google.maps.MapTypeId.ROADMAP
-    });    
+    });
     var mePinColor = "0000FF";
     var mePinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_icon&chld=home|" + mePinColor,
         new google.maps.Size(21, 34),
-        new google.maps.Point(0,0),
+        new google.maps.Point(0, 0),
         new google.maps.Point(10, 34));
     var mePinShadow = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_shadow",
         new google.maps.Size(40, 37),
         new google.maps.Point(0, 0),
         new google.maps.Point(12, 35));
-    bounds.extend (myLatlng);
+    bounds.extend(myLatlng);
     var marker = new google.maps.Marker({
-          position: myLatlng,
-          map: map,
-          icon: mePinImage,
-          shadow: mePinShadow,
-          title: 'You'
+        position: myLatlng,
+        map: map,
+        icon: mePinImage,
+        shadow: mePinShadow,
+        title: 'You'
     });
-        
+
     var lat = myLatlng.lat();
-    var lng = myLatlng.lng();  
+    var lng = myLatlng.lng();
     //queryLocations(map, lat, lng);
     //setMarkers(map, placesArray);
-    
-    var Place = StackMob.Model.extend({ schemaName: 'locations' });
-    var Places = StackMob.Collection.extend({ model: Place });
+
+    var Place = StackMob.Model.extend({
+        schemaName: 'locations'
+    });
+    var Places = StackMob.Collection.extend({
+        model: Place
+    });
     var distance = $("#mileSelect input:radio:checked").val();
-    
+
     var q = new StackMob.Collection.Query();
-    q.mustBeNearMi('lat_long', new StackMob.GeoPoint(lat,lng), distance);
-    q.select('name','lat_long');
-      
+    q.mustBeNearMi('lat_long', new StackMob.GeoPoint(lat, lng), distance);
+    q.select('name', 'lat_long');
+
     var placesContainer = new Places();
     placesContainer.query(q, {
-      success: function(model, locations) {
-        var infowindow = new google.maps.InfoWindow({content: ''});
-        for(var i = 0; i < locations.length; i++ ) {
-            var myLatLng = new google.maps.LatLng(locations[i].lat_long.lat, locations[i].lat_long.lon);
-            bounds.extend (myLatLng);
-            var marker = new google.maps.Marker({
-              position: myLatLng,
-              map: map,
-              title: locations[i].name,
-              zIndex: i
+        success: function (model, locations) {
+            var infowindow = new google.maps.InfoWindow({
+                content: ''
             });
-            map.fitBounds(bounds);
-            buildInfoWindow(infowindow,marker,map,locations[i]);
+            for (var i = 0; i < locations.length; i++) {
+                var myLatLng = new google.maps.LatLng(locations[i].lat_long.lat, locations[i].lat_long.lon);
+                bounds.extend(myLatLng);
+                var marker = new google.maps.Marker({
+                    position: myLatLng,
+                    map: map,
+                    title: locations[i].name,
+                    zIndex: i
+                });
+                map.fitBounds(bounds);
+                buildInfoWindow(infowindow, marker, map, locations[i]);
+            }
+
+        },
+        error: function (model, response) {
+            console.log(response);
         }
-        
-      },
-      error: function(model, response) {
-        console.log(response);
-      }
     });
 
-    
+
 }
 
-function buildInfoWindow(infowindow,marker,map,locations){
-    var contentString = '<div id="infoWindow">'+
-                            '<a href="#details" data-transition="slide"><h3>'+locations.name+'</h3></a>'+
-                            '<div id="infoWindowBody">'+
-                            'Howdy'+
-                            '</div>'+
-                    '</div>';
-                
+function buildInfoWindow(infowindow, marker, map, locations) {
+    var contentString = '<div id="infoWindow">' +
+        '<a href="#details" data-transition="slide"><h3>' + locations.name + '</h3></a>' +
+        '<div id="infoWindowBody">' +
+        'Howdy' +
+        '</div>' +
+        '</div>';
+
     //var infowindow = new google.maps.InfoWindow({
     //    content: contentString
     //});
-        google.maps.event.addListener(marker, 'click', function() {
+    google.maps.event.addListener(marker, 'click', function () {
         infowindow.content = contentString;
-        infowindow.open(map,marker);
+        infowindow.open(map, marker);
         console.log(marker);
     });
 }
 //function queryLocations(map, lat, lng) {
-  
+
 //}
 
 /*function queryLocations(lat, lng) {
