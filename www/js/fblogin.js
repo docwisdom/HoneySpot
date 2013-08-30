c9abe3d9d11ffd9fd58a9889bb6902fefa81278b
docwisdom <brian@docwisdom.com>
2013-08-30 10:33:11 -0700

    Facebook login

--- www/js/fblogin.js
+++ www/js/fblogin.js
@@ -1,9 +1,26 @@
-FB.login(function(response) {
-	if(response.authResponse) {
-      var accessToken = response.authResponse.accessToken;
-      Scrumptious.stackmobLogin(response.authResponse.userID, accessToken);
-    }
-});
+  // Additional JS functions here
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '172523219600825 ', // App ID
+      channelUrl : '//APP.HONEYSPOTAPP.COM/channel.html', // Channel File
+      status     : true, // check login status
+      cookie     : true, // enable cookies to allow the server to access the session
+      xfbml      : true  // parse XFBML
+    });
+
+    // Additional init code here
+
+  };
+
+  // Load the SDK asynchronously
+  (function(d){
+     var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement('script'); js.id = id; js.async = true;
+     js.src = "//connect.facebook.net/en_US/all.js";
+     ref.parentNode.insertBefore(js, ref);
+   }(document));
+
 
 
  
